8cace57e5bd65875a5307c99942c355d71f8fe50
e74f0c5876f447a689c71b99113f4e65 <17622871777@163.com>
2024-08-14 00:02:19 +0800

    Initial commit

---
+++ src/setting.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+    proxyUrl:'http://localhost:8088',
+    title:"旅游信息管理系统",
+}
---
+++ src/views/liuyanban/webadd.vue
@@ -0,0 +1,17 @@
+<template>
+  <e-container>
+    <Add></Add>
+  </e-container>
+</template>
+<style type="text/scss" lang="scss">
+
+</style>
+<script>
+import Add from './add'
+
+export default {
+  components: {
+    'Add': Add
+  },
+}
+</script>
